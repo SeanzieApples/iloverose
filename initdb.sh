dc647c6f1e503d88641541b1db7611c44288c354
sean hale <shale@tetrazoid.net>
2017-12-20 18:36:53 -0600

    changing db init script

--- initdb.sh
+++ initdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it mysql bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /initdb/init.sql'
+docker exec -it testhost bash -c 'mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /initdb/init.sql'
